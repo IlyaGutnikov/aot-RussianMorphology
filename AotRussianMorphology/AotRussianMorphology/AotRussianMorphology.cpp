ff4660654ba988a60267804d50168de23ae3b340
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-05-23 15:27:37 +0300

    Добавил проект

---
+++ AotRussianMorphology/AotRussianMorphology/AotRussianMorphology.cpp
@@ -0,0 +1,14 @@
+// AotRussianMorphology.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib> // для system
+using namespace std;
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+	//system("pause"); // Только для тех, у кого MS Visual Studio
+	return 0;
+}
+
