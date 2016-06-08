01e6c2b558e83f1e79f0fa1fd6178da64773a255
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-06-08 11:48:20 +0300

    Добавил тестовое C++ приложение и добавил проверки в C# приложение

---
+++ LemmataizerClient/LemmataizerClient/LemmataizerClient.cpp
@@ -0,0 +1,21 @@
+// LemmataizerClient.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <comdef.h>
+#include "windows.h"
+#include <iostream>
+#include <process.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World";
+	system("pause");
+
+    return 0;
+}
+
--- TestLemmatizerCSharp/TestLemmatizerCSharp/TestLemmatizerCSharp/Program.cs
+++ TestLemmatizerCSharp/TestLemmatizerCSharp/TestLemmatizerCSharp/Program.cs
@@ -11,48 +11,60 @@ namespace TestLemmatizerCSharp
     {
         public static string inputWord = "";
         public static string inputCommand = "";
+        public static bool check = false;
 
         static void Main(string[] args)
         {
             if (args.Length > 0)
             {
                 inputCommand = args[0];
-                inputWord = args[1];
+                if (args.Length > 1)
+                {
+                    inputWord = args[1];
+                    check = true;
+                }
             }
 
             else {
 
                 Console.WriteLine("error");
+                check = false;
             }
-
-            //создали лемматайзер
-            AOTLemmatizer.AOTLemmatizer aotLem = new AOTLemmatizer.AOTLemmatizer();
-
-            if (inputCommand.Equals("GetAllForms"))
+            if (check == true)
             {
-                List<string> forms = aotLem.GetAllForms(inputWord).ToList();
-                for (int k = 0; k < forms.Count; k++)
+                //создали лемматайзер
+                AOTLemmatizer.AOTLemmatizer aotLem = new AOTLemmatizer.AOTLemmatizer();
+
+                if ((!inputCommand.Equals("GetAllNorms")) && (!inputCommand.Equals("GetAllForms")) && (!inputCommand.Equals("Stem")))
                 {
-                    Console.WriteLine(forms[k].ToString());
+                    Console.WriteLine("error");
+                }
+
+                if (inputCommand.Equals("GetAllForms"))
+                {
+                    List<string> forms = aotLem.GetAllForms(inputWord).ToList();
+                    for (int k = 0; k < forms.Count; k++)
+                    {
+                        Console.WriteLine(forms[k].ToString());
+                    }
+                }
+
+                if (inputCommand.Equals("GetAllNorms"))
+                {
+                    List<string> norms = aotLem.GetAllNorms(inputWord).ToList();
+                    for (int k = 0; k < norms.Count; k++)
+                    {
+                        Console.WriteLine(norms[k].ToString());
+                    }
+                }
+
+                if (inputCommand.Equals("Stem"))
+                {
+                    string stem = aotLem.Stem(inputWord);
+                    Console.WriteLine(stem);
                 }
             }
-
-            if (inputCommand.Equals("GetAllNorms"))
-            {
-                List<string> norms = aotLem.GetAllNorms(inputWord).ToList();
-                for (int k = 0; k < norms.Count; k++)
-                {
-                    Console.WriteLine(norms[k].ToString());
-                }
-            }
-
-            if (inputCommand.Equals("Stem"))
-            {
-                string stem = aotLem.Stem(inputWord);
-                Console.WriteLine(stem);
-            }
-
-            Console.ReadLine();
+            //Console.ReadLine();
 
         }
     }
